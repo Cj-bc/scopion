51083e692dabdc9028f6e1a58ed11af508b64dc4
Cj <cj-bc@y-modify.org>
2018-01-27 11:53:41 +0900

    change url to download

--- scopion.rb
+++ scopion.rb
@@ -5,7 +5,7 @@
 class Scopion < Formula
 	desc "a statically-typed functional programming language with powerful objective syntax"
 	homepage "https://scopion.coord-e.com/"
-	url "https://github/coord-e/scopion.git"
+	url "https://github.com/Cj-bc/homebrew-scopion.git"
 	version "0.0.3.1"
 	sha256 ""
 
